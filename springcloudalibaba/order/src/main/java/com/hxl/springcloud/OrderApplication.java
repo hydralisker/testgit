50d6eae7ee939fdac2b59b988888bb5b9535be28
icebear <hxl0422@sina.com>
2022-07-16 18:07:14 +0800

    create project

---
+++ springcloudalibaba/order/src/main/java/com/hxl/springcloud/OrderApplication.java
@@ -0,0 +1,21 @@
+package com.hxl.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+import javax.annotation.Resource;
+
+@SpringBootApplication
+public class OrderApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(OrderApplication.class,args);
+    }
+    @Bean
+    public RestTemplate restTemplate(RestTemplateBuilder builder){
+        RestTemplate build = builder.build();
+        return build;
+    }
+}
---
+++ springcloudalibaba/order/src/main/java/com/hxl/springcloud/controller/OrderController.java
@@ -0,0 +1,19 @@
+package com.hxl.springcloud.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@RestController
+@RequestMapping("/order")
+public class OrderController {
+    @Autowired
+    RestTemplate restTemplate;
+    @RequestMapping("/add")
+    public String add() {
+        String message = restTemplate.getForObject("http://localhost:9090/stock/reduct", String.class);
+        System.out.println("下单成功！");
+        return "下单成功！" +message;
+    }
+}
