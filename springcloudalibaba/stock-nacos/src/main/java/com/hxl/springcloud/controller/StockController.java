5b6c8610b7cda98475490dcd3454addd8bc88dc0
hydralisker <hxl0422@sina.com>
2022-07-16 21:57:01 +0800

    nacos

---
+++ springcloudalibaba/stock-nacos/src/main/java/com/hxl/springcloud/controller/StockController.java
@@ -0,0 +1,17 @@
+package com.hxl.springcloud.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/stock")
+public class StockController {
+    @Value("${server.port}")
+    String port;
+    @RequestMapping("/reduct")
+    public String reduct(){
+        System.out.println("扣减库存");
+        return "扣减库存,端口："+port;
+    }
+}
